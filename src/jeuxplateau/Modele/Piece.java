01b422c320b78774556f118b49669a5dc4763ae8
CyprienF <floquet.cyprien@gmail.com>
2018-04-10 11:24:30 +0200

    Ajout classe Piece

--- src/jeuxplateau/Modele/Piece.java
+++ src/jeuxplateau/Modele/Piece.java
@@ -10,5 +10,53 @@ package jeuxplateau.Modele;
  * @author Stefano
  */
 public class Piece {
-    
+    private int orientation;
+    private int idPiece;
+    private int[][] matricePiece;
+    private Joueur joueur;
+    private int pointOrientation;
+
+    public Piece() {
+
+    }
+
+    public int getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(int orientation) {
+        this.orientation = orientation;
+    }
+
+    public int getIdPiece() {
+        return idPiece;
+    }
+
+    public void setIdPiece(int idPiece) {
+        this.idPiece = idPiece;
+    }
+
+    public int[][] getMatricePiece() {
+        return matricePiece;
+    }
+
+    public void setMatricePiece(int[][] matricePiece) {
+        this.matricePiece = matricePiece;
+    }
+
+    public Joueur getJoueur() {
+        return joueur;
+    }
+
+    public void setJoueur(Joueur joueur) {
+        this.joueur = joueur;
+    }
+
+    public int getPointOrientation() {
+        return pointOrientation;
+    }
+
+    public void setPointOrientation(int pointOrientation) {
+        this.pointOrientation = pointOrientation;
+    }
 }
