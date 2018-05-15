c528ef5c94e8d5f926ecc2bfe5dba700c2e44590
CyprienF <floquet.cyprien@gmail.com>
2018-05-15 11:31:22 +0200

    Ajout de la grille Tetris

--- src/jeuxplateau/Vue/TetrisView.java
+++ src/jeuxplateau/Vue/TetrisView.java
@@ -2,29 +2,45 @@ package jeuxplateau.Vue;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-
-import static javafx.application.Application.launch;
 
 public class TetrisView {
 
     private Stage primaryStage;
     private Scene plateauxTetris;
     private Group root;
+    private GridPane grille;
+    private GridPane piece;
+
+    private static int HAUTEUR_GRILLE = 20;
+    private static int LARGEUR_GRILLE = 10;
 
     public TetrisView(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.root = new Group();
 
-        Button btn = new Button();
-        btn.setText("aaa");
+        grille = new GridPane();
+        for (int ligne = 0; ligne < HAUTEUR_GRILLE; ligne++) {
+            for (int colonne = 0; colonne < LARGEUR_GRILLE; colonne++) {
+                Rectangle rec = new Rectangle();
+                rec.setWidth(25);
+                rec.setHeight(25);
+                rec.setFill(Color.LIGHTGRAY);
+                rec.setStroke(Color.GRAY);
+                GridPane.setRowIndex(rec, ligne);
+                GridPane.setColumnIndex(rec, colonne);
+                grille.getChildren().addAll(rec);
+            }
+        }
 
-        this.root.getChildren().add(btn);
+        this.root.getChildren().add(grille);
 
-        this.plateauxTetris = new Scene(root, 800, 600);
+        this.plateauxTetris = new Scene(root, 425, 525);
+
         primaryStage.setScene(plateauxTetris);
-
-
+        primaryStage.setTitle("Tetris");
     }
 }
